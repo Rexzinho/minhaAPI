85a527938d81819449e04eaca08a45661ae05b6b
Rexzinhoo <viniciussorgetzalves@gmail.com>
2024-06-08 15:09:30 -0300

    Atualizando arquivos

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const FileController = require("../controllers/FileController");
+const checkTokenByFolder = require("../middleware/checkTokenByFolder");
 
 module.exports = router;
 
-router.post("/create", FileController.create)
+router.post("/create", checkTokenByFolder, FileController.create)
